b2edf0b64bec6ada829d189ddebf8a39ec57d655
Adam <adamk22@gmail.com>
2024-11-20 02:30:29 +0700

    Update

--- lib/classes/workouts/index.dart
+++ lib/classes/workouts/index.dart
@@ -2,4 +2,4 @@ export 'custom_workout.class.dart';
 export 'pacer_workout.class.dart';
 export 'single_goal_workout.class.dart';
 export 'swim_bike_run_workout.class.dart';
-export 'workout.class.dart';
+export 'base_workout.class.dart';
